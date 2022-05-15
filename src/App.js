52ade65c05686fa5de766f164bdd3380830c702f
Your Name <you@example.com>
2022-05-15 19:31:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+import Preview from './assesment1/Preview';
+
+function App() {
+  return (
+    <div className="App">
+      <Preview />
+    </div>
+  );
+}
+
+export default App;
